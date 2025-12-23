64cea82f048c85d639eecbcd7d778925a4e34db8
Kalana-indula <kalanasmedia@outlook.com>
2025-12-23 18:28:00 +0530

    dashboard added

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,9 +1,18 @@
-import { ResponsiveLine } from "@nivo/line";
-import { useTheme } from "@mui/material";
-import { tokens } from "../theme";
-import { mockLineData as data } from "../data/mockData";
+import React from "react";
+import {ResponsiveLine} from "@nivo/line";
+import {useTheme} from "@mui/material";
+import {tokens} from "../theme";
+import {mockLineData as data} from "../data/mockData";
 
-const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
+type LineChartProps = {
+    isCustomLineColors?: boolean;
+    isDashboard?: boolean;
+};
+
+const LineChart: React.FC<LineChartProps> = ({
+                                                 isCustomLineColors = false,
+                                                 isDashboard = false,
+                                             }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
@@ -12,40 +21,25 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
             data={data}
             theme={{
                 axis: {
-                    domain: {
-                        line: {
-                            stroke: colors.grey[100],
-                        },
-                    },
-                    legend: {
-                        text: {
-                            fill: colors.grey[100],
-                        },
-                    },
+                    domain: {line: {stroke: colors.grey[100]}},
+                    legend: {text: {fill: colors.grey[100]}},
                     ticks: {
-                        line: {
-                            stroke: colors.grey[100],
-                            strokeWidth: 1,
-                        },
-                        text: {
-                            fill: colors.grey[100],
-                        },
+                        line: {stroke: colors.grey[100], strokeWidth: 1},
+                        text: {fill: colors.grey[100]},
                     },
                 },
-                legends: {
-                    text: {
-                        fill: colors.grey[100],
-                    },
-                },
-                tooltip: {
-                    container: {
-                        color: colors.primary[500],
-                    },
-                },
+                legends: {text: {fill: colors.grey[100]}},
+                tooltip: {container: {color: colors.primary[500]}},
             }}
-            colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }} // added
-            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-            xScale={{ type: "point" }}
+            colors={
+                isDashboard
+                    ? {datum: "color"}
+                    : isCustomLineColors
+                        ? {datum: "color"}
+                        : {scheme: "nivo"}
+            }
+            margin={{top: 50, right: 110, bottom: 50, left: 60}}
+            xScale={{type: "point"}}
             yScale={{
                 type: "linear",
                 min: "auto",
@@ -58,32 +52,30 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
             axisTop={null}
             axisRight={null}
             axisBottom={{
-                orient: "bottom",
                 tickSize: 0,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: isDashboard ? undefined : "transportation", // added
+                legend: isDashboard ? undefined : "transportation",
                 legendOffset: 36,
                 legendPosition: "middle",
             }}
             axisLeft={{
-                orient: "left",
-                tickValues: 5, // added
+                tickValues: 5,
                 tickSize: 3,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: isDashboard ? undefined : "count", // added
+                legend: isDashboard ? undefined : "count",
                 legendOffset: -40,
                 legendPosition: "middle",
             }}
             enableGridX={false}
             enableGridY={false}
             pointSize={8}
-            pointColor={{ theme: "background" }}
+            pointColor={{theme: "background"}}
             pointBorderWidth={2}
-            pointBorderColor={{ from: "serieColor" }}
+            pointBorderColor={{from: "serieColor"}}
             pointLabelYOffset={-12}
-            useMesh={true}
+            useMesh
             legends={[
                 {
                     anchor: "bottom-right",
